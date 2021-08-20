dfedcf74299934e95be9b4f5d177334e7759cf7c
Nurul_Huda <mohnurulhuda32@gmail.com>
2021-08-20 15:04:06 +0700

    membuat register baru

--- resources/views/layout/v_template.blade.php
+++ resources/views/layout/v_template.blade.php
@@ -59,7 +59,7 @@
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <img src="{{asset('template/')}}/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-              <span class="hidden-xs">Alexander Pierce</span>
+              <span class="hidden-xs"> {{ Auth::user()->name }}</span>
             </a>
             <ul class="dropdown-menu">
               <!-- User image -->
@@ -67,23 +67,13 @@
                 <img src="{{asset('template/')}}/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
 
                 <p>
-                  Alexander Pierce - Web Developer
-                  <small>Member since Nov. 2012</small>
+                  {{ Auth::user()->name }} -  {{ Auth::user()->email }}
+                  {{-- <small>Member since Nov. 2012</small> --}}
                 </p>
               </li>
               <!-- Menu Body -->
               <li class="user-body">
-                <div class="row">
-                  <div class="col-xs-4 text-center">
-                    <a href="#">Followers</a>
-                  </div>
-                  <div class="col-xs-4 text-center">
-                    <a href="#">Sales</a>
-                  </div>
-                  <div class="col-xs-4 text-center">
-                    <a href="#">Friends</a>
-                  </div>
-                </div>
+              
                 <!-- /.row -->
               </li>
               <!-- Menu Footer-->
@@ -92,7 +82,11 @@
                   <a href="#" class="btn btn-default btn-flat">Profile</a>
                 </div>
                 <div class="pull-right">
-                  <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                  <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                    <button type="submit" class="btn btn-default btn-flat">Log out</a>
+                </form>
+                 
                 </div>
               </li>
             </ul>
@@ -115,7 +109,7 @@
           <img src="{{asset('template/')}}/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
-          <p>Alexander Pierce</p>
+          <p> {{ Auth::user()->name }}</p>
           <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
         </div>
       </div>
--- resources/views/v_home.blade.php
+++ resources/views/v_home.blade.php
@@ -2,7 +2,6 @@
 @section('title','Home')
 
 @section('content')
-    <h1>SMK Gondang</h1>
-    <h4>{{ $nama_sekolah }}</h4><br>
-    <h3>{{ $alamat }}</h3>
+  
+  
 @endsection
